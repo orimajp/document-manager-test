1855d4b857095715cb94f330df39ab700830e386
Hideo Oshima <orimajp@gmail.com>
2020-08-09 19:43:56 +0900

    登録/更新日付表示暫定実装

--- docman-frontend2/app/components/view/DocumentViewContent.vue
+++ docman-frontend2/app/components/view/DocumentViewContent.vue
@@ -1,8 +1,13 @@
 <template>
   <v-main>
-    <v-container>
+    <v-container fluid>
       <div ref="viewer">
         <h1 class="document-title">{{ pageTitle }}</h1>
+        <div class="markdown-content">
+          <div class="date-data">
+            {{ contentDate }}
+          </div>
+        </div>
         <div class="markdown-body">
           <div v-html="$md.render(pageData)" />
         </div>
@@ -13,6 +18,7 @@
 
 <script lang="ts">
 import {
+  computed,
   defineComponent,
   nextTick,
   onMounted,
@@ -31,7 +37,13 @@ export default defineComponent({
     pageContent: Object as PropType<PageData>
   },
   setup(props: PageContentProp) {
-    const { pageTitle, pageData } = useViewContent(props)
+    const {
+      pageTitle,
+      pageData,
+      createdAt,
+      updatedAt,
+      formatDate
+    } = useViewContent(props)
 
     const viewer = ref(null) as Ref<HTMLElement | null> // ref=viewer相当
     useCollectHeadline(props, viewer)
@@ -51,10 +63,19 @@ export default defineComponent({
       removeNavigateListener()
     })
 
+    const contentDate = computed(
+      // () => `作成: ${createdAt.value} 更新: ${updatedAt.value}`
+      () =>
+        `作成: ${formatDate(createdAt.value)} 更新: ${formatDate(
+          updatedAt.value
+        )}`
+    )
+
     return {
       viewer,
       pageTitle,
-      pageData
+      pageData,
+      contentDate
     }
   }
 })
--- docman-frontend2/app/hooks/view/viewContentHook.ts
+++ docman-frontend2/app/hooks/view/viewContentHook.ts
@@ -1,5 +1,8 @@
 import { computed } from '@vue/composition-api'
+/// import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 import { PageData } from '~/models/page/PageData'
+import 'dayjs/locale/ja'
 
 export interface PageContentProp {
   pageContent: PageData
@@ -9,12 +12,27 @@ export const useViewContent = (props: PageContentProp) => {
   const pageTitle = computed(() =>
     props.pageContent === null ? '' : props.pageContent.pageTitle
   )
+
   const pageData = computed(() =>
     props.pageContent === null ? '' : props.pageContent.pageData
   )
 
+  const createdAt = computed(() =>
+    props.pageContent === null ? '' : props.pageContent.createdAt
+  )
+
+  const updatedAt = computed(() =>
+    props.pageContent === null ? '' : props.pageContent.updatedAt
+  )
+
+  const formatDate = (date: string) =>
+    dayjs(date).format('YYYY年MM月DD日 HH:mm:ss')
+
   return {
     pageTitle,
-    pageData
+    pageData,
+    createdAt,
+    updatedAt,
+    formatDate
   }
 }
