f97ec917ff41ca3ab56a5dea04cfeb0f5c875188
Hideo Oshima <orimajp@gmail.com>
2020-09-08 00:31:08 +0900

    ライブラリバージョンアップ他

--- docman-backend/src/index/IndexFactory.ts
+++ docman-backend/src/index/IndexFactory.ts
@@ -3,6 +3,8 @@ import { Index } from '~/index/index';
 const remark = require('remark')
 const strip = require('strip-markdown')
 
+// https://koirand.github.io/blog/2018/pulp-search/
+// https://koirand.github.io/blog/2018/pulp-search2/
 export function createIndex(page: IPage): Index {
   const ref = createRef(page)
   const body = createBody(page)
