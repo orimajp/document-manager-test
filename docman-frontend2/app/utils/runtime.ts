7e073fa288f853f7a78fa690b36ac12d98e85cf9
Hideo Oshima <orimajp@gmail.com>
2020-12-20 23:07:11 +0900

    ライブラリバージョンアップ

--- docman-frontend2/app/utils/runtime.ts
+++ docman-frontend2/app/utils/runtime.ts
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import { getCurrentInstance } from '@vue/composition-api'
 
 export const getRuntimeVM = (): Vue => {
-  const vm = getCurrentInstance()
+  const vm = getCurrentInstance()?.proxy
   if (vm) {
     return vm
   }
